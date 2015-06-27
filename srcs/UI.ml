7a88fbe063f8506220cfaa681ae3700a4429ae88
jaguillo <juloo.dsi@gmail.com>
2015-06-27 17:00:59 +0200

    basic ui

--- srcs/UI.ml
+++ srcs/UI.ml
@@ -6,7 +6,7 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/27 15:07:56 by jaguillo          #+#    #+#             *)
-(*   Updated: 2015/06/27 15:22:29 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2015/06/27 17:00:48 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -15,9 +15,9 @@ class virtual basic_object x y w h = object
 	val _y = y
 	val _width = w
 	val _height = h
-	method virtual on_click :basic_object
-	method virtual draw :unit
-	method virtual update :basic_object
+	method on_click (x:int) (y:int) (env:Data.data) = {< >}
+	method update (env:Data.data) = {< >}
+	method virtual draw :Data.data -> unit
 	method x = _x
 	method y = _y
 	method width = _width
@@ -26,4 +26,24 @@ class virtual basic_object x y w h = object
 		let right = _x + _width in
 		let bottom = _y + _height in
 		x >= _x && y >= _y && x < right && y < bottom
+end
+
+class group x y w h childs = object
+	inherit basic_object x y w h
+	val _childs = childs
+	method on_click x y (env:Data.data) = {< _childs = List.map (fun c ->
+			if c#is_in x y then
+				c#on_click x y env
+			else
+				c
+		) _childs >}
+	method draw (env:Data.data) = List.iter (fun c -> c#draw env) _childs
+	method update (env:Data.data) = {< _childs = List.map (fun c ->
+			c#update env
+		) _childs >}
+end
+
+class text x y w h text = object
+	inherit basic_object x y w h
+	method draw (env:Data.data) = ()
 end
