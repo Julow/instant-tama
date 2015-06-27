1a060a9fc82626f2a5f93e3a6f30cccbd9188e3a
Nicolas GOGUEY <ngoguey@e1r5p20.42.fr>
2015-06-27 18:58:44 +0200

    added stats/actions handling functions

--- srcs/Stat.ml
+++ srcs/Stat.ml
@@ -6,9 +6,11 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/27 17:53:27 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/27 18:20:56 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/06/27 18:54:50 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
+
+type t = float array
 
 let details =
   [|
@@ -22,3 +24,9 @@ let details =
 let default_status =
   [|100.; 100.; 100.; 100.|]
 
+let n = Array.length details
+
+(** elapsed: elapsed miliseconds (sdl time)
+ ** sv: Stats values *)
+let apply_decay elapsed sv: t =
+  Array.mapi (fun i v -> let (_, _, d) = details.(i) in v -. d) sv
