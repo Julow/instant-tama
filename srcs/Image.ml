cfda9fe22e78f415e1e467894c6e8cec91c15090
Nicolas GOGUEY <ngoguey@e1r5p20.42.fr>
2015-06-27 15:31:51 +0200

    added main file

--- srcs/Image.ml
+++ srcs/Image.ml
@@ -6,11 +6,12 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/27 15:05:37 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/27 15:13:22 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/06/27 15:29:06 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-type dat = {tid: int; filename: string; w: int; h: int; sdlptr: Sdlvideo.surface}
+type dat = {tid: int; filename: string; w: int; h: int;
+			sdlptr: Sdlvideo.surface}
 
 type t = Loaded of dat | Failure of string
 
--- srcs/main.ml
+++ srcs/main.ml
@@ -3,11 +3,32 @@
 (*                                                        :::      ::::::::   *)
 (*   main.ml                                            :+:      :+:    :+:   *)
 (*                                                    +:+ +:+         +:+     *)
-(*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
+(*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
-(*   Created: 2015/06/27 15:03:43 by jaguillo          #+#    #+#             *)
-(*   Updated: 2015/06/27 15:03:53 by jaguillo         ###   ########.fr       *)
+(*   Created: 2015/06/27 15:19:59 by ngoguey           #+#    #+#             *)
+(*   Updated: 2015/06/27 15:29:27 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-let () = ()
+let show img dst =
+  match img with
+  | Image.Failure _		-> ()
+  | Image.Loaded dat		->
+	 let d = Sdlvideo.display_format dat.sdlptr in
+	 Sdlvideo.blit_surface d dst ();
+	 Sdlvideo.flip dst
+
+let rec mainloop ((display, img) as env) =
+  show img display;
+  mainloop env
+
+let () =
+  Printf.printf "Hello world\n%!";
+  (* Load SDL *)
+  Sdl.init [`EVERYTHING];
+  Sdlevent.enable_events Sdlevent.all_events_mask;
+  let display = Sdlvideo.set_video_mode 500 500 [`DOUBLEBUF] in
+  (* Init les datas *)
+  (* Main loop *)
+  let img = Image.load_texture "ressources/Icons.jpg" 0 in
+  mainloop (display, img)
