fb4210e453f530af12948b1eca61a71bd413d0b9
jaguillo <juloo.dsi@gmail.com>
2015-06-28 17:33:43 +0200

    Bound stats values

--- srcs/Stat.ml
+++ srcs/Stat.ml
@@ -6,29 +6,29 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/27 17:53:27 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/28 17:00:31 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/06/28 17:32:26 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 type t = float array
 
-let details =
-  [|
+let details = [|
 	(* (INDEX, NAME, LOSSPERSEC) *)
 	(0, "Health", 1.);
 	(1, "Energy", 0.);
 	(2, "Hygiene", 0.);
 	(3, "Happyness", 0.);
-  |]
+|]
 
-let default_status () =
-  [|100.; 100.; 100.; 100.|]
+let default_status () = [|100.; 100.; 100.; 100.|]
 
 let n = Array.length details
 
 (** elapsed: elapsed miliseconds (sdl time)
  ** sv: Stats values *)
 let apply_decay elapsed (sv: t) : t =
-  let ef = float elapsed in
-  Array.mapi (fun i v -> let (_, _, d) = details.(i) in
-						 v -. (d *. ef /. 1000.)) sv
+	let ef = float elapsed in
+	Array.mapi (fun i v ->
+			let (_, _, d) = details.(i) in
+			max (min (v -. (d *. ef /. 1000.)) 100.) 0.
+		) sv
